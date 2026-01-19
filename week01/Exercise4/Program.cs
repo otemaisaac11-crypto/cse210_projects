5f7b277c4f21704ec5973baa982ee9b71898c018
Isaac Otema <otemaisaac11@gmai.com>
2026-01-19 13:17:58 +0300

    Lists

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,42 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a number (0 to quit):");
+        int number = int.Parse(Console.ReadLine());
+        while (number != 0)
+        {
+            numbers.Add(number);
+            Console.WriteLine("Enter a number (0 to quit):");
+            number = int.Parse(Console.ReadLine());
+        }
+        if (numbers.Count == 0)
+        {
+            Console.WriteLine("No numbers were entered.");
+            return;
+        }
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"The sum of numbers is: {sum}");
+
+        double average = (double)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
