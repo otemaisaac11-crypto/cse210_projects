9a3b72daae957da41bf12cdfa833aa7e305089e2
Isaac Otema <otemaisaac11@gmai.com>
2026-01-20 15:59:21 +0300

    final exercise

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,47 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-<<<<<<< HEAD
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
-=======
-        DisplayMessage();
+        
+        DisplayWelcome();
 
-        string name = PromptUserName();
-        int number = PromptUserNumber();
-        int square = SquareNumber(number);
-        DisplayResult(name, square);
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(favoriteNumber);
+
+        DisplayResult(userName, squaredNumber);
     }
 
-    static void DisplayMessage()
+    static void DisplayWelcome()
     {
         Console.WriteLine("Welcome to the program!");
     }
 
+    
     static string PromptUserName()
     {
         Console.Write("Please enter your name: ");
         return Console.ReadLine();
     }
 
+    
     static int PromptUserNumber()
     {
         Console.Write("Please enter your favorite number: ");
-        int number;
-        while (!int.TryParse(Console.ReadLine(), out number))
-        {
-            Console.Write("Invalid number. Please enter your favorite number: ");
-        }
-        return number;
+        return int.Parse(Console.ReadLine());
     }
 
+    
     static int SquareNumber(int number)
     {
         return number * number;
     }
 
-    static void DisplayResult(string name, int square)
+
+    static void DisplayResult(string name, int squaredNumber)
     {
-        Console.WriteLine($"{name}, the square of your number is {square}");
->>>>>>> b04a32b (Save changes)
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
     }
 }
