0e7e93141b40eb6f8d873cd89055f09aba2e4714
Isaac Otema <otemaisaac11@gmai.com>
2026-01-20 15:40:48 +0300

    Save changes

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
+<<<<<<< HEAD
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+=======
+        DisplayMessage();
+
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+        DisplayResult(name, square);
+    }
+
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number;
+        while (!int.TryParse(Console.ReadLine(), out number))
+        {
+            Console.Write("Invalid number. Please enter your favorite number: ");
+        }
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+>>>>>>> b04a32b (Save changes)
     }
 }
